c081dce62ba513c1a5ad0553d4e7573f2dc22a91
maxi <gigenamaximo@gmail.com>
2024-12-15 07:10:25 -0300

    segundo commit <3

---
+++ OperacionesMatemáticasBásicas.php
@@ -0,0 +1,14 @@
+<?php
+//hola soy un comentario 
+//como estas?
+$primernumero = 1;
+$segundonumero = 2; 
+$resultado = $GLOBALS["primernumero"] + $GLOBALS["segundonumero"];
+print $resultado ."\n";
+
+$resultado = $GLOBALS["primernumero"] - $GLOBALS["segundonumero"];
+print $resultado ."\n";
+
+$resultado = $GLOBALS["primernumero"] * $GLOBALS["segundonumero"];
+print $resultado ."\n";
+?>
